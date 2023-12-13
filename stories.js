2480cd07f21882a93fa32b97269a7c1c5b916657
Huijin <huba0001@stud.kea.dk>
2023-12-13 15:39:14 +0100

    Stories sider er færdige

--- stories.js
+++ stories.js
@@ -2,13 +2,3 @@ document.getElementById("burgerIcon").addEventListener("click", function () {
   var navbar = document.getElementById("myNavbar");
   navbar.classList.toggle("show");
 });
-
-//værdier
-function toggleAnswer(answerId) {
-  var answer = document.getElementById(answerId);
-  if (answer.style.display === "block") {
-    answer.style.display = "none";
-  } else {
-    answer.style.display = "block";
-  }
-}
